82322d8d9894f7b3e1b5b504483d4d11748c8a72
Ekostromin <Ekostromin@IBS>
2022-10-26 13:32:01 +0400

    2.16. Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,17 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+import { Offer } from '../offer-card/offer-card';
+
+type AppPageSettings = {
+  cardsCount: number;
+  offers: Offer[];
+}
+
+function App(appPageSettings: AppPageSettings): JSX.Element {
+  const cardsCount = appPageSettings.cardsCount;
+  const offers = appPageSettings.offers;
+  return(
+    <MainPage cardsCount={cardsCount} offers={offers} />
+  );
 }
 
 export default App;
