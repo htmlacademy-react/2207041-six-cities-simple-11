f7854c7e158948d7f23a985eb70eabf22a0110e0
Ekostromin <Ekostromin@IBS>
2022-11-06 19:46:21 +0400

    Обратная сторона реальности

--- project/src/pages/property-page/property-page.tsx
+++ project/src/pages/property-page/property-page.tsx
@@ -1,8 +1,11 @@
 import { useParams } from 'react-router-dom';
+import ReviewPlace from '../../components/review-place/review-place';
+import { offers } from '../../mocks/offers';
 
 function PropertyPage(): JSX.Element {
   const params = useParams();
   const id: string = params.id ?? ''; // Добавлено чтобы в будущем делать выборку из списка по id b отображения
+  const offer = offers.find((item) => item.id.toString() === id);
   return(
     <html lang="en">
       <head>
@@ -88,6 +91,7 @@ function PropertyPage(): JSX.Element {
                   <div className="property__name-wrapper">
                     <h1 className="property__name" title={`offer/${id}`}>
                       Beautiful &amp; luxurious studio at great location
+                      {offer?.description}
                     </h1>
                   </div>
                   <div className="property__rating rating">
@@ -109,8 +113,8 @@ function PropertyPage(): JSX.Element {
                     </li>
                   </ul>
                   <div className="property__price">
-                    <b className="property__price-value">&euro;120</b>
-                    <span className="property__price-text">&nbsp;night</span>
+                    <b className="property__price-value">{offer?.price.cost}</b>
+                    <span className="property__price-text">{offer?.price.additionalInfo}</span>
                   </div>
                   <div className="property__inside">
                     <h2 className="property__inside-title">What&apos;s inside</h2>
@@ -169,79 +173,7 @@ function PropertyPage(): JSX.Element {
                       </p>
                     </div>
                   </div>
-                  <section className="property__reviews reviews">
-                    <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-                    <ul className="reviews__list">
-                      <li className="reviews__item">
-                        <div className="reviews__user user">
-                          <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                            <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
-                          </div>
-                          <span className="reviews__user-name">
-                            Max
-                          </span>
-                        </div>
-                        <div className="reviews__info">
-                          <div className="reviews__rating rating">
-                            <div className="reviews__stars rating__stars">
-                              <span style={{width: '80%'}}></span>
-                              <span className="visually-hidden">Rating</span>
-                            </div>
-                          </div>
-                          <p className="reviews__text">
-                            A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                          </p>
-                          <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                        </div>
-                      </li>
-                    </ul>
-                    <form className="reviews__form form" action="/#" method="post">
-                      <label className="reviews__label form__label" htmlFor="review">Your review</label>
-                      <div className="reviews__rating-form form__rating">
-                        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"/>
-                        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                          <svg className="form__star-image" width="37" height="33">
-                            <use xlinkHref="#icon-star"></use>
-                          </svg>
-                        </label>
-
-                        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"/>
-                        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                          <svg className="form__star-image" width="37" height="33">
-                            <use xlinkHref="#icon-star"></use>
-                          </svg>
-                        </label>
-
-                        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"/>
-                        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                          <svg className="form__star-image" width="37" height="33">
-                            <use xlinkHref="#icon-star"></use>
-                          </svg>
-                        </label>
-
-                        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"/>
-                        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                          <svg className="form__star-image" width="37" height="33">
-                            <use xlinkHref="#icon-star"></use>
-                          </svg>
-                        </label>
-
-                        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"/>
-                        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                          <svg className="form__star-image" width="37" height="33">
-                            <use xlinkHref="#icon-star"></use>
-                          </svg>
-                        </label>
-                      </div>
-                      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
-                      <div className="reviews__button-wrapper">
-                        <p className="reviews__help">
-                          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-                        </p>
-                        <button className="reviews__submit form__submit button" type="submit" disabled={false}>Submit</button>
-                      </div>
-                    </form>
-                  </section>
+                  <ReviewPlace />
                 </div>
               </div>
               <section className="property__map map"></section>
--- project/src/types/constants.tsx
+++ project/src/types/constants.tsx
@@ -6,7 +6,26 @@ export enum AppRoute {
   Main = '/main',
   LoginPage = '/login',
   Root = '/',
-  PropertyPage = '/offer/:id',
+  Properties = '/offer/',
+  PropertyPage = ':id',
   MainEmptyPage = '/main-empty-page',
   PropertyNotLoggedPage = '/property-not-logged-page'
+}
+
+export enum Rating {
+  One = '20%',
+  Two = '40%',
+  Three = '60%',
+  Four = '80%',
+  Five = '100%'
+}
+
+export enum DayNight {
+  Day = '',
+  Night = 'night'
+}
+
+export enum ApartmentType {
+  Apartment = 'Apartment',
+  PrivateRoom = 'Private room'
 }
